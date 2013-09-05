d1c56132f6ca26af0db5116782665025a719444d
Shelby Munsch <smunsch@gmail.com>
2013-09-05 11:06:32 -0700

    Set up Vagrant (vagrantup.com) and simple nginx testing configuration.

---
+++ .config/provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y nginx
+# TODO: set up nginx config
+cp -f /vagrant/.config/nginx/default /etc/nginx/sites-enabled/default
+rm -r /usr/share/nginx/www
+ln -s /vagrant /usr/share/nginx/www
