1c79b4735e390138f71cf61d6e19279f889b64f6
Shelby Munsch <smunsch@gmail.com>
2013-09-12 00:05:38 -0700

    fixed some nginx config and start at launch (derp)

--- .config/provision.sh
+++ .config/provision.sh
@@ -3,6 +3,9 @@
 apt-get update
 apt-get install -y nginx
 # TODO: set up nginx config
-cp -f /vagrant/.config/nginx/default /etc/nginx/sites-enabled/default
+
+cp -f /vagrant/.config/nginx/default /etc/nginx/sites-available/default
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 rm -r /usr/share/nginx/www
 ln -s /vagrant /usr/share/nginx/www
+/etc/init.d/nginx start
